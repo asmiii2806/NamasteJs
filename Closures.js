6c0cb0455d3921c82127eb4d178e6a32c8eccc1c
asmiii2806 <goelap02@gmail.com>
2024-02-12 12:49:27 +0530

    closures

---
+++ Closures.js
@@ -0,0 +1,24 @@
+
+function x()
+{
+    var a=7;
+    function y()
+    {
+        console.log(a);
+    }
+    return y;
+
+}
+
+var z=x();
+console.log(z);
+
+
+
+/*output:
+function y()
+{
+    console.log(a);
+}
+7
+*/
