434a41aefbc3e8cf1d28544a26799750633eae07
asmiii2806 <goelap02@gmail.com>
2024-02-29 19:43:37 +0530

    dom manipulation

--- practice.js
+++ practice.js
@@ -5,3 +5,19 @@ const asmi=(para1, para2)=>
 }
 
 asmi();
+
+let arr=["pune", "delhi", "mumbai"];
+arr.forEach((val, idx, arr)=>{          //tske callback func as parameter
+    console.log(val, idx, arr);
+});
+
+
+let nums=[2,3,4,5];
+
+nums.forEach((num)=>
+{
+    console.log(num*num);
+    
+});
+
+console.dir(window.document);
